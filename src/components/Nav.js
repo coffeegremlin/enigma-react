ad175746c2e631ffdc17b74c0f96781adb919eb1
Nate <natemausert@gmail.com>
2022-06-08 17:33:58 -0700

    add folder structure a bit, add dependencies, create Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <div className="navbar">
+            <div>
+                <NavLink to='/'>Lamp Board</NavLink>
+            </div>
+            <div>
+                <NavLink to='/plugboard'>Plug Board</NavLink>
+            </div>
+            <div>
+                <NavLink to='/messenger'>Enigma Messenger</NavLink>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
